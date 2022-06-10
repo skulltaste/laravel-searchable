4c9ebf9d75b44c008a1069ef332438fc1eb27b4e
skulltaste <Brick6863@gmail.com>
2022-06-10 09:38:27 -0500

    update

--- src/ModelSearchAspect.php
+++ src/ModelSearchAspect.php
@@ -151,7 +151,7 @@ class ModelSearchAspect extends SearchAspect
         $with = $this->with;
 
         //$searchTerms = explode(' ', $term);
-        if($this->constraint_column) {
+        if(count($this->constraint_column) > 0) {
             $constraint_columns = $this->constraint_column;
             $constraint_values = $this->constraint_value;
             foreach ($constraint_columns as $key => $constraint_column) {
